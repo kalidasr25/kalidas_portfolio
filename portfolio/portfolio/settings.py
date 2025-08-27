9c3acfb7bb574b8f92c6b5d6ba167cee5b845caf
kalidasr25 <kalidas.sr23@gmail.com>
2025-08-27 19:42:11 +0530

    Fix STATIC_ROOT path for Render

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "main/static"]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
